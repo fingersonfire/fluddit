cecaa2d09345649c758d45447e8526223d354b0e
Stone Pack <stone.pack@outlook.com>
2021-07-30 21:57:33 -0700

    cleaned up main widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,21 @@
-import 'package:fluddit/bloc/reddit.bloc.dart';
-import 'package:fluddit/routes/feed.dart';
+import 'package:fluddit/bloc/index.dart';
+import 'package:fluddit/components/index.dart';
+import 'package:fluddit/components/top.appbar.dart';
+import 'package:fluddit/routes/index.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_nord_theme/flutter_nord_theme.dart';
 import 'package:get/get.dart';
+
+GlobalKey<ScaffoldState> scaffoldKey = new GlobalKey<ScaffoldState>();
 
 void main() {
   runApp(App());
 }
 
 class App extends StatelessWidget {
-  RedditController c = Get.put(RedditController());
+  final ComponentController comp = Get.put(ComponentController());
+  final RedditController reddit = Get.put(RedditController());
 
-  @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
       themeMode: ThemeMode.dark,
@@ -19,38 +23,15 @@ class App extends StatelessWidget {
       darkTheme: NordTheme.dark(),
       title: 'fluddit',
       home: Scaffold(
-        bottomNavigationBar: BottomAppBar(
-          child: Row(
-            children: [
-              IconButton(icon: Icon(Icons.menu), onPressed: () {}),
-              Spacer(),
-              IconButton(
-                onPressed: () {
-                  c.getSubredditPosts(
-                    sub: 'flutter',
-                    listing: 'hot',
-                    limit: 50,
-                  );
-                },
-                icon: Icon(Icons.refresh_outlined),
-              ),
-              IconButton(icon: Icon(Icons.search_outlined), onPressed: () {}),
-            ],
-          ),
+        key: scaffoldKey,
+        bottomNavigationBar: bottomAppBar(
+          comp,
+          reddit,
+          scaffoldKey,
         ),
-        appBar: AppBar(
-          toolbarHeight: 35,
-          backgroundColor: Colors.indigo[300],
-          brightness: Brightness.dark,
-          centerTitle: true,
-          title: Text('Frontpage'),
-          leading: Container(),
-        ),
-        body: Feed(
-          listing: 'hot',
-          subreddit: 'flutter',
-        ),
-        drawer: Drawer(),
+        appBar: topAppBar(reddit),
+        body: Feed(),
+        drawer: subredditsDrawer(),
       ),
     );
   }
