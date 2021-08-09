33d99c78e9a3272116c3db7cef3dcb03960066da
Stone Pack <stone.pack@outlook.com>
2021-08-08 18:35:16 -0700

    resolved final issue with fetching permenant auth token

--- lib/bloc/auth.bloc.dart
+++ lib/bloc/auth.bloc.dart
@@ -1,18 +1,48 @@
+import 'dart:convert';
+
 import 'package:fluddit/secrets.dart';
+import 'package:http/http.dart' as HTTP;
 import 'package:get/get.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class AuthController extends GetxController {
-  /// Gets Reddit auth token using the code provided in the redirect uri.
-  Future setAuthToken(String url) async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
+  final Uri authUrl = Uri.parse('https://www.reddit.com/api/v1/access_token');
 
-    final String _query = url.split('$redirectUri/#')[1];
+  Future refreshAuthToken() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    // TODO: Add request to fetch new bearer using refresh_token.
+  }
+
+  /// Gets Reddit auth token.
+  Future setAuthToken(String url) async {
+    // Starts the getInstance function while making time consuming requests.
+    Future<SharedPreferences> prefs = SharedPreferences.getInstance();
+
+    final String _query = url.split('$redirectUri/')[1];
     final Map<String, dynamic> _result = Uri.splitQueryString(_query);
 
-    await prefs.setString('access_token', _result['access_token']);
-    await prefs.setString('expires_in', _result['expires_in']);
+    final HTTP.Response _resp = await HTTP.post(
+      authUrl,
+      headers: {
+        'Authorization': 'basic $basicAuth',
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'User-Agent': userAgent,
+      },
+      body: {
+        'grant_type': 'authorization_code',
+        'redirect_uri': redirectUri,
+        // Figuring out the split was needed to get a working code took quite
+        // a bit of time. Shout out to iOS for making it easy to setup proxy
+        // certs and other projects for figuring out the code before me.
+        'code': _result['code'].split('#_')[0],
+      },
+    );
 
-    print(prefs.getString('access_token'));
+    Map<String, dynamic> _json = jsonDecode(_resp.body);
+
+    // Await the results of the getInstance function called earlier.
+    SharedPreferences instance = await prefs;
+    await instance.setString('access_token', _json['access_token']);
+    await instance.setString('refresh_token', _json['refresh_token']);
   }
 }
