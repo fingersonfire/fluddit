1b7aada5d8ce4cc84fe1ab2e7d9d924cd834ceeb
Stone Pack <stone.pack@outlook.com>
2021-08-08 18:40:50 -0700

    initial setup for subreddit list in drawer

--- lib/components/subreddits.drawer.dart
+++ lib/components/subreddits.drawer.dart
@@ -1,13 +1,40 @@
+import 'package:fluddit/bloc/reddit.bloc.dart';
+import 'package:fluddit/models/index.dart';
 import 'package:fluddit/routes/login.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 Drawer subredditsDrawer() {
+  final RedditController reddit = Get.find();
+
   return Drawer(
     child: Column(
       children: [
         Expanded(
-          child: Container(),
+          child: Container(
+            child: Container(
+              color: Colors.transparent,
+              child: Obx(
+                () => ListView.builder(
+                  itemCount: reddit.options['subscribed'].length,
+                  itemBuilder: (context, i) {
+                    final Subreddit subreddit =
+                        reddit.options['subscribed'][i] as Subreddit;
+
+                    return TextButton(
+                      onPressed: () {
+                        reddit.options['subreddit'] = subreddit.name;
+                        reddit.feedPosts.clear();
+                        reddit.getInitPosts(limit: 50);
+                        Get.back();
+                      },
+                      child: Text(subreddit.name),
+                    );
+                  },
+                ),
+              ),
+            ),
+          ),
         ),
         Container(
           color: Colors.black87,
