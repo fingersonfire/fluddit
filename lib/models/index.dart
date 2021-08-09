d1634110377438d327c4cba86fad227a753b55fd
Stone Pack <stone.pack@outlook.com>
2021-08-08 18:41:13 -0700

    created model for subreddit object

--- lib/models/index.dart
+++ lib/models/index.dart
@@ -1,1 +1,3 @@
 export 'post.model.dart';
+
+export 'subreddit.model.dart';
---
+++ lib/models/subreddit.model.dart
@@ -0,0 +1,31 @@
+class Subreddit {
+  final String bannerImage;
+  final String description;
+  final String flair;
+  final String headerImage;
+  final String name;
+  final bool nsfw;
+  final bool subscribed;
+
+  Subreddit({
+    required this.bannerImage,
+    required this.description,
+    required this.flair,
+    required this.headerImage,
+    required this.name,
+    required this.nsfw,
+    required this.subscribed,
+  });
+
+  factory Subreddit.fromJson(Map<String, dynamic> json) {
+    return Subreddit(
+      bannerImage: json['banner_img'],
+      description: json['public_description'],
+      flair: '${json['user_flair_text']}',
+      headerImage: '${json['header_img']}',
+      name: json['display_name'],
+      nsfw: json['over18'],
+      subscribed: json['user_is_subscriber'],
+    );
+  }
+}
