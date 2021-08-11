48526a09b14c8992d41a60ff442b539578f0e72e
Stone Pack <stone.pack@outlook.com>
2021-08-11 07:05:59 -0700

    added funciton for hanlding post type thumbnails

--- lib/bloc/component.bloc.dart
+++ lib/bloc/component.bloc.dart
@@ -1,3 +1,6 @@
+import 'package:fluddit/components/index.dart';
+import 'package:fluddit/models/index.dart';
+import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 
 class ComponentController extends GetxController {
@@ -10,6 +13,20 @@ class ComponentController extends GetxController {
   void closeDrawer(scaffoldKey) {
     if (scaffoldKey.currentState.isDrawerOpen) {
       scaffoldKey.currentState.closeDrawer();
+    }
+  }
+
+  Widget getPostThumbnail(RedditPost post) {
+    if (post.isSelf) {
+      return Center(
+        child: Text('Tt'),
+      );
+    } else if (post.isVideo) {
+      return VideoThumbnail(post: post);
+    } else if (post.domain == 'i.redd.it') {
+      return ImageThumbnail(post: post);
+    } else {
+      return WebThumbnail(post: post);
     }
   }
 }
