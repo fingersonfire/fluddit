1b1de087e4eb0ec685b98ca539f82cff42e5891a
Stone Pack <stone.pack@outlook.com>
2021-08-11 22:01:15 -0700

    added gesture detector to post tiles

--- lib/components/feed/post.tile.dart
+++ lib/components/feed/post.tile.dart
@@ -1,5 +1,6 @@
 import 'package:fluddit/bloc/index.dart';
 import 'package:fluddit/models/index.dart';
+import 'package:fluddit/routes/post.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -22,38 +23,46 @@ class PostTile extends StatelessWidget {
       ),
       margin: EdgeInsets.only(top: 10, left: 10, right: 10),
       width: MediaQuery.of(context).size.width,
-      child: ListTile(
-        leading: Container(
-          width: 30,
-          height: 50,
-          child: Center(
-            child: Text(
-              reddit.getScoreString(post.score),
+      child: GestureDetector(
+        onTap: () {
+          Get.to(
+            PostView(),
+            transition: Transition.rightToLeft,
+          );
+        },
+        child: ListTile(
+          leading: Container(
+            width: 30,
+            height: 50,
+            child: Center(
+              child: Text(
+                reddit.getScoreString(post.score),
+              ),
             ),
           ),
-        ),
-        title: Text(
-          post.title,
-          maxLines: 2,
-          style: TextStyle(
-            color: post.stickied ? Colors.green : null,
+          title: Text(
+            post.title,
+            maxLines: 2,
+            style: TextStyle(
+              color: post.stickied ? Colors.green : null,
+            ),
+            overflow: TextOverflow.ellipsis,
           ),
-          overflow: TextOverflow.ellipsis,
-        ),
-        subtitle: Text(
-          'r/${post.subreddit} | ${post.author}',
-          style: TextStyle(
-            fontSize: 10,
-            color: Theme.of(context).hintColor,
+          subtitle: Text(
+            'r/${post.subreddit} | ${post.author}',
+            style: TextStyle(
+              fontSize: 10,
+              color: Theme.of(context).hintColor,
+            ),
           ),
-        ),
-        trailing: Container(
-          decoration: BoxDecoration(
-            color: Theme.of(context).canvasColor,
+          trailing: Container(
+            decoration: BoxDecoration(
+              color: Theme.of(context).canvasColor,
+            ),
+            height: 50,
+            width: 50,
+            child: comp.getPostThumbnail(post),
           ),
-          height: 50,
-          width: 50,
-          child: comp.getPostThumbnail(post),
         ),
       ),
     );
