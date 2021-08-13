beb78bd9c4cc4edc21458ada792c6e877b4c7694
Stone Pack <stone.pack@outlook.com>
2021-08-13 16:56:28 -0700

    added comments box to post view

--- lib/routes/post.dart
+++ lib/routes/post.dart
@@ -1,15 +1,19 @@
+import 'package:fluddit/bloc/index.dart';
 import 'package:fluddit/components/index.dart';
 import 'package:fluddit/models/index.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class PostView extends StatelessWidget {
-  const PostView({Key? key, required this.post}) : super(key: key);
+  PostView({Key? key, required this.post}) : super(key: key);
 
   final RedditPost post;
 
+  final RedditController reddit = Get.find();
+
   @override
   Widget build(BuildContext context) {
+    print(post.id);
     return Scaffold(
       appBar: AppBar(
         toolbarHeight: 40,
@@ -35,10 +39,25 @@ class PostView extends StatelessWidget {
                   ),
                   // Comments
                   Container(
-                    width: MediaQuery.of(context).size.width,
-                    height: 300,
-                    child: Center(
-                      child: Text('Comments not yet implemented'),
+                    child: FutureBuilder(
+                      builder: (context, AsyncSnapshot snapshot) {
+                        if (snapshot.connectionState == ConnectionState.done) {
+                          return CommentList(comments: snapshot.data);
+                        }
+                        return Container(
+                          width: MediaQuery.of(context).size.width,
+                          height: 300,
+                          child: Center(
+                            child: CircularProgressIndicator(
+                              color: Colors.indigo[300],
+                            ),
+                          ),
+                        );
+                      },
+                      future: reddit.getPostComments(
+                        subreddit: post.subreddit,
+                        postId: post.id,
+                      ),
                     ),
                   ),
                 ],
