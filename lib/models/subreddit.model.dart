c7b3f3582bd768443a41d728fdc4414bfdc51887
Stone Pack <stone.pack@outlook.com>
2021-08-14 13:50:32 -0700

    mapped subreddit icon data

--- lib/models/subreddit.model.dart
+++ lib/models/subreddit.model.dart
@@ -3,6 +3,7 @@ class Subreddit {
   final String description;
   final String? flair;
   final String? headerImage;
+  final String icon;
   final String name;
   final bool nsfw;
   final bool subscribed;
@@ -12,6 +13,7 @@ class Subreddit {
     required this.description,
     required this.flair,
     required this.headerImage,
+    required this.icon,
     required this.name,
     required this.nsfw,
     required this.subscribed,
@@ -23,6 +25,7 @@ class Subreddit {
       description: json['public_description'],
       flair: json['user_flair_text'],
       headerImage: json['header_img'],
+      icon: json['community_icon'],
       name: json['display_name'],
       nsfw: json['over18'],
       subscribed: json['user_is_subscriber'],
