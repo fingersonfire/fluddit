023d3828da06beadf75b7c336796434dbf33d542
Stone <stone.pack@outlook.com>
2021-08-16 19:14:55 -0700

    cleaned up drawer subreddit button

--- lib/components/drawer/subreddit.button.dart
+++ lib/components/drawer/subreddit.button.dart
@@ -1,38 +1,29 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:fluddit/bloc/index.dart';
 import 'package:fluddit/components/drawer/subreddit.icon.dart';
 import 'package:fluddit/models/index.dart';
+import 'package:fluddit/routes/index.dart';
 import 'package:flutter/material.dart';
 
 class SubredditButton extends StatelessWidget {
-  const SubredditButton({Key? key, required this.subreddit}) : super(key: key);
+  SubredditButton({Key? key, required this.subreddit}) : super(key: key);
 
   final Subreddit subreddit;
+
+  final SubredditController controller = Get.find();
 
   @override
   Widget build(BuildContext context) {
     return MaterialButton(
       child: Row(
         children: [
-          subreddit.icon == ''
-              ? SubredditIcon()
-              : CachedNetworkImage(
-                  imageUrl: subreddit.icon.replaceAll('&amp;', '&'),
-                  imageBuilder: (context, imageProvider) => Container(
-                    child: SubredditIcon(image: imageProvider),
-                  ),
-                  placeholder: (context, url) => SubredditIcon(),
-                  errorWidget: (context, url, error) => SubredditIcon(),
-                ),
+          SubredditIcon(subreddit: subreddit),
           Text(subreddit.name),
         ],
       ),
       onPressed: () {
-        final RedditController reddit = Get.find();
-
-        reddit.options['subreddit'] = subreddit.name;
-        reddit.getInitPosts(limit: 50);
+        controller.name.value = subreddit.name;
         Get.back();
+        Get.to(() => SubredditView(subreddit: subreddit));
       },
     );
   }
