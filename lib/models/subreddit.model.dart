2c1200b08889aa5ed838b8db56415596c381c645
Stone <stone.pack@outlook.com>
2021-08-16 19:18:11 -0700

    mapped additional data to subreddit model

--- lib/models/subreddit.model.dart
+++ lib/models/subreddit.model.dart
@@ -1,22 +1,26 @@
 class Subreddit {
-  final String bannerImage;
+  final String? bannerImage;
   final String description;
   final String? flair;
+  final String fullName;
   final String? headerImage;
   final String icon;
   final String name;
-  final bool nsfw;
+  final bool? nsfw;
   final bool subscribed;
+  final String title;
 
   Subreddit({
     required this.bannerImage,
     required this.description,
     required this.flair,
+    required this.fullName,
     required this.headerImage,
     required this.icon,
     required this.name,
     required this.nsfw,
     required this.subscribed,
+    required this.title,
   });
 
   factory Subreddit.fromJson(Map<String, dynamic> json) {
@@ -24,11 +28,13 @@ class Subreddit {
       bannerImage: json['banner_img'],
       description: json['public_description'],
       flair: json['user_flair_text'],
+      fullName: json['name'],
       headerImage: json['header_img'],
       icon: json['community_icon'],
       name: json['display_name'],
       nsfw: json['over18'],
       subscribed: json['user_is_subscriber'],
+      title: json['title'],
     );
   }
 }
