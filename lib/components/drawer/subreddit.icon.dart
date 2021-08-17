8da32f5b5c55f501802de41a58233d8e3e1da6cf
Stone <stone.pack@outlook.com>
2021-08-16 19:15:28 -0700

    created circle avatar for subreddits icons

--- lib/components/drawer/subreddit.icon.dart
+++ lib/components/drawer/subreddit.icon.dart
@@ -1,12 +1,15 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:fluddit/models/subreddit.model.dart';
+import 'package:fluddit/widgets/conditional.widget.dart';
 import 'package:flutter/material.dart';
 
 class SubredditIcon extends StatelessWidget {
   SubredditIcon({
     Key? key,
-    this.image = const AssetImage('lib/assets/redditIcon.png'),
+    required this.subreddit,
   }) : super(key: key);
 
-  final image;
+  final Subreddit subreddit;
 
   @override
   Widget build(BuildContext context) {
@@ -14,10 +17,33 @@ class SubredditIcon extends StatelessWidget {
       margin: EdgeInsets.only(right: 10),
       width: 30,
       height: 30,
-      child: CircleAvatar(
-        backgroundImage: image,
-        backgroundColor: Colors.white24,
+      child: ConditionalWidget(
+        condition: subreddit.icon == '',
+        trueWidget: SubredditPlaceholderIcon(),
+        falseWidget: CachedNetworkImage(
+          imageUrl: subreddit.icon.replaceAll('&amp;', '&'),
+          imageBuilder: (context, imageProvider) => Container(
+            child: CircleAvatar(
+              backgroundImage: imageProvider,
+              backgroundColor: Colors.white24,
+            ),
+          ),
+          placeholder: (context, url) => SubredditPlaceholderIcon(),
+          errorWidget: (context, url, error) => SubredditPlaceholderIcon(),
+        ),
       ),
+    );
+  }
+}
+
+class SubredditPlaceholderIcon extends StatelessWidget {
+  const SubredditPlaceholderIcon({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CircleAvatar(
+      backgroundImage: AssetImage('lib/assets/redditIcon.png'),
+      backgroundColor: Colors.white24,
     );
   }
 }
