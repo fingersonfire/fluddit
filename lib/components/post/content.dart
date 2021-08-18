fe50f9a96a0a5d81257841e7409dc7387db349f3
Stone <stone.pack@outlook.com>
2021-08-17 17:29:41 -0700

    updated image content to handle imgur links

--- lib/components/post/content.dart
+++ lib/components/post/content.dart
@@ -1,3 +1,5 @@
+import 'package:fluddit/bloc/index.dart';
+import 'package:fluddit/components/post/gallery.content.dart';
 import 'package:fluddit/components/post/video.content.dart';
 import 'package:fluddit/components/post/web.content.dart';
 import 'package:fluddit/models/index.dart';
@@ -17,12 +19,14 @@ class ContentBox extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: constraints.maxHeight - 115,
-      child: getContent(post),
+      child: getContent(post, constraints),
     );
   }
 }
 
-Widget getContent(RedditPost post) {
+Widget getContent(RedditPost post, BoxConstraints constraints) {
+  final ComponentController component = Get.find();
+
   if (post.isSelf) {
     return SingleChildScrollView(
       child: Container(
@@ -34,12 +38,14 @@ Widget getContent(RedditPost post) {
       ),
     );
   } else if (post.isVideo) {
-    return VideoContent(url: '${post.url}/DASH_1080.mp4');
-  } else if (post.domain == 'i.redd.it') {
+    return VideoContent(url: '${post.url}/DASH_480.mp4');
+  } else if (component.isImage(post.url) || post.domain == 'imgur.com') {
     return Image.network(
-      '${post.url}',
+      post.domain == 'imgur.com' ? '${post.url}.jpg' : post.url ?? '',
       fit: BoxFit.contain,
     );
+  } else if (post.isGallery) {
+    return GalleryContent(constraints: constraints, post: post);
   } else {
     return WebContent(url: '${post.url}');
   }
